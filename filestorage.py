1849ad02399ac2355adb71792f01a82a9b73c34e
Alexander Todorov <atodorov@dif.io>
2014-03-07 00:48:09 +0200

    redefine _save() as well

--- filestorage.py
+++ filestorage.py
@@ -16,7 +16,9 @@
 #
 ################################################################################
 
+import os
 from django.conf import settings
+from django.core.files import locks
 from django.core.files.storage import FileSystemStorage
 
 
@@ -33,3 +35,53 @@ class OverwriteFileSystemStorage(FileSystemStorage):
             return name
         return super(OverwriteFileSystemStorage, self).get_available_name(name)
 
+    def _save(self, name, content):
+        """ This is similar to the parent calss but removes any safe-guards against existing files """
+        if not self.file_overwrite:
+            return super(OverwriteFileSystemStorage, self)._save(name, content)
+
+        full_path = self.path(name)
+
+        # Create any intermediate directories that do not exist.
+        # Note that there is a race between os.path.exists and os.makedirs:
+        # if os.makedirs fails with EEXIST, the directory was created
+        # concurrently, and we can continue normally. Refs #16082.
+        directory = os.path.dirname(full_path)
+        if not os.path.exists(directory):
+            try:
+                os.makedirs(directory)
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise
+        if not os.path.isdir(directory):
+            raise IOError("%s exists and is not a directory." % directory)
+
+        # This file has a file path that we can move.
+        if hasattr(content, 'temporary_file_path'):
+            file_move_safe(content.temporary_file_path(), full_path)
+            content.close()
+
+        # This is a normal uploadedfile that we can stream.
+        else:
+            flags = (os.O_WRONLY | os.O_CREAT | getattr(os, 'O_BINARY', 0))
+            # The current umask value is masked out by os.open!
+            fd = os.open(full_path, flags, 0o666)
+            _file = None
+            try:
+                locks.lock(fd, locks.LOCK_EX)
+                for chunk in content.chunks():
+                    if _file is None:
+                        mode = 'wb' if isinstance(chunk, bytes) else 'wt'
+                        _file = os.fdopen(fd, mode)
+                    _file.write(chunk)
+            finally:
+                locks.unlock(fd)
+                if _file is not None:
+                    _file.close()
+                else:
+                    os.close(fd)
+
+        if settings.FILE_UPLOAD_PERMISSIONS is not None:
+            os.chmod(full_path, settings.FILE_UPLOAD_PERMISSIONS)
+
+        return name
